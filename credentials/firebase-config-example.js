4e7d021d4e96d68d6aeb6964aab9196636f49940
Carlos Tovar <cartovarc@gmail.com>
2020-05-18 23:56:30 -0500

    add example credentials

---
+++ credentials/firebase-config-example.js
@@ -0,0 +1,13 @@
+// this file must be called "firebase-config.js"
+
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+export { firebaseConfig };
